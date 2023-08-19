0d94f12a5a09aaf8f660162ad9ede33d81a524ba
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2023-08-20 01:33:15 +0300

    Progress on electron

--- electron/main.ts
+++ electron/main.ts
@@ -32,8 +32,12 @@ process.env.PORT = '9494';
 })();
 
 // const { app, BrowserWindow } = require('electron');
+// (async () => {
+// 	await import('../build/index.js');
+// })();
+// // require('../build/index.js')(module);
 // // await import('../build/index.js');
-//
+
 // const createWindow = () => {
 // 	const mainWindow = new BrowserWindow({
 // 		width: 1200,
@@ -42,20 +46,20 @@ process.env.PORT = '9494';
 // 			nodeIntegration: true
 // 		}
 // 	});
-//
+
 // 	mainWindow.loadURL('http://localhost:9494');
 // };
-//
+
 // app.whenReady().then(() => {
 // 	createWindow();
-//
+
 // 	app.on('activate', () => {
 // 		// On macOS it's common to re-create a window in the app when the
 // 		// dock icon is clicked and there are no other windows open.
 // 		if (BrowserWindow.getAllWindows().length === 0) createWindow();
 // 	});
 // });
-//
+
 // app.on('window-all-closed', () => {
 // 	if (process.platform !== 'darwin') app.quit();
 // });
--- forge.config.cjs
+++ forge.config.cjs
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
 	packagerConfig: {
 		asar: true
@@ -26,5 +29,18 @@ module.exports = {
 			name: '@electron-forge/plugin-auto-unpack-natives',
 			config: {}
 		}
-	]
+	],
+	hooks: {
+		packageAfterPrune: async (forgeConfig, buildPath, electronVersion, platform, arch) => {
+			if (platform === 'darwin') {
+				console.log('We need to remove the problematic link file on macOS');
+				console.log(`Build path ${buildPath}`);
+				try {
+					fs.unlinkSync(path.join(buildPath, 'node_modules/sqlite3/build/node_gyp_bins/python3'));
+				} catch (err) {
+					console.error(err);
+				}
+			}
+		}
+	}
 };
