01ca893e040450490bd0934eca7f38f053dbc20c
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2023-08-24 17:02:17 +0300

    Working macos and windows builds

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
